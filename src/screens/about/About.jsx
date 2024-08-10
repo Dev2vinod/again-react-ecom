81f816f529c29bb3cf04844e5c3e37f269154595
Dev2vinod <vinodsinghfun@gmail.com>
2024-08-10 23:37:39 +0530

    simple routing done
    i have made a simple routing done

---
+++ src/screens/about/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='bg-orange-400'>What you think about your self</div>
+  )
+}
+
+export default About
---
+++ src/screens/contact/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='bg-violet-600'>I am contact page </div>
+  )
+}
+
+export default Contact
