6a2fc088a32ffa1382f332e67c9ac049e8b25374
Dev2vinod <vinodsinghfun@gmail.com>
2024-08-11 01:40:27 +0530

    all product has been shown

---
+++ src/component/CardProduct.jsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import ReactStars from 'react-stars'
+
+import './top.css'
+export default function MediaCard({product}) {
+
+console.log("product in the card",product)
+  return (
+    <Card sx={{ width:300 ,marginTop:10}}>
+      <div>
+        <img src={product.image}  className='w-[200px] h-[200px] object-contain mt-3 m-2' alt="" />
+      </div>
+      <CardContent>
+
+      <Typography gutterBottom variant="h6" component="div">
+       
+       Rs {product.price} /-
+       </Typography>
+        <Typography gutterBottom variant="span" component="div">
+          {product.title.slice(0,32)}...
+      
+        </Typography>
+        
+        <ReactStars
+            edit={false}
+            count={5}
+            value={product.rating.rate}
+             size={24}
+            color2={'#ffd700'} />
+
+
+        
+      </CardContent>
+      <CardActions>
+        <Button size="small" variant="contained" color="success"  className='addBtn'>Add To Cart</Button>
+        <Button size="small" variant="outlined"  className='viewBtn'>View Detail</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import DrawerAppBar from '../../component/Topbar'
+import MediaCard from '../../component/CardProduct'
+import axios from 'axios'
 
 const Home = () => {
+
+  const[data,setData] =useState([])
+   
+  useEffect(() => {
+
+    axios.get(`https://fakestoreapi.com/products`).then((res)=>{
+        
+        console.log(res.data,"res")
+        setData(res.data)
+        // setData([...data])
+        console.log(data,"from  data")
+    }).catch((err)=>{
+        console.log("error from fake store api" ,err)
+    })
+
+    
+  }, [])
+  
+
   return (
-    <div>Home</div>
+    <div>
+        <DrawerAppBar />
+
+        <div className='flex bg-pink-500 flex-wrap justify-evenly items-center '>
+        
+        {data.length >0 ? data.map((item,i)=>{
+           return  <MediaCard  product ={item} key={i} />
+
+        }):<h1> data is loading</h1> }
+        
+        </div>
+    </div>
   )
 }
 
